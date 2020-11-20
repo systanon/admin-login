0d9880f29a23e7cc32d3715f98a87c250762f242
sergei <tustanovskiis@gmail.com>
2020-11-20 13:52:50 +0200

    start

---
+++ src/components/Login.vue
@@ -0,0 +1,84 @@
+<template>
+  <v-form ref="form" class="form" align="center">
+    <v-text-field v-model="login" :rules="[rules.required]" label="login" />
+    <div class="input-pass">
+      <v-text-field
+        v-model="password"
+        :type="showPass ? 'text' : 'password'"
+        :rules="[rules.required]"
+        label="password"
+      />
+      <v-checkbox label="Show" class="show-pass pa-0 ma-0" v-model="showPass"
+        >show</v-checkbox
+      >
+    </div>
+    <v-btn class="mt-8" @click="submit">submit</v-btn>
+  </v-form>
+</template>
+
+<script>
+  export default {
+    name: 'Login',
+    data: () => ({
+      login: '',
+      password: '',
+      showPass: false,
+      rules: {
+        required: (v) => !!v || 'input is required',
+      },
+    }),
+    methods: {
+      async sendData(data) {
+        const res = await fetch(`${process.env.VUE_APP_API_URL}/admin/login`, {
+          method: 'POST',
+          mode: 'cors',
+          cache: 'no-cache',
+          credentials: 'same-origin',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          redirect: 'follow',
+          referrerPolicy: 'no-referrer',
+          body: JSON.stringify({
+            ...data,
+          }),
+        });
+        if (res.status === 200) {
+          console.log(res.headers.get('Set-Cookie'))
+          //  window.location.href = `${process.env.VUE_APP_API_URL}/admin`;
+        }
+       
+      },
+      submit() {
+        const data = {
+          email: this.login,
+          password: this.password,
+        };
+        if (this.$refs.form.validate()) {
+          this.sendData(data);
+        }
+      },
+    },
+  };
+</script>
+<style>
+.form {
+  position: fixed;
+  width: 300px;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+.input-pass {
+  position: relative;
+}
+.show-pass {
+  position: absolute;
+  bottom: 0;
+  right: 0;
+}
+.v-label,
+.v-icon.v-icon {
+  font-size: 14px !important;
+}
+</style>
